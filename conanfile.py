6a779b86cbd02eae929d4b27a7ef20df98c06c97
vovs <vgovseychuk@gmail.com>
2026-07-14 06:41:12 +0000

    WIP. Build OK

--- conanfile.py
+++ conanfile.py
@@ -7,11 +7,6 @@ class TrafficSim(ConanFile):
     generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
-        self.requires("sdl/3.4.8")
-        self.requires("imgui/1.92.8")
-        self.requires("nlohmann_json/3.12.0")
-        self.requires("gtest/1.17.0")
-        self.requires("benchmark/1.9.5")
         self.requires("quill/12.0.0")
 
     def layout(self):
