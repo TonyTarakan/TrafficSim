7a90a4399270847bb8378958f8cdfda729be0d67
vovs <vgovseychuk@gmail.com>
2026-07-29 21:17:46 +0000

    spawn draft

--- include/core/spawn.hpp
+++ include/core/spawn.hpp
@@ -2,23 +2,40 @@
 
 #include <vector>
 
+#include "core/sim_engine.hpp"
 #include "core/types.hpp"
 
 namespace ts {
 
+struct Destination {
+    NodeId node;
+    float weight;  // probability
+};
+
 struct SpawnPoint {
     NodeId origin;
-    std::vector<NodeId> destinations;
-    float spawn_rate{1.f};  // vehicles per second or tick counter?
+    std::vector<Destination> destinations;
+    float spawn_rate{1.f};  // vehicles per second
     // TODO(later):
-    // - std::vector<???> dst_probability;
     // - VehicleType distribution
     // - start_time;
     // - end_time;
 };
 
 // TODO
+class SpawnSystem {
+public:
+    explicit SpawnSystem(SimEngine& engine);
+    void update(float dt);
+    void spawn_once(NodeId from, NodeId to, int count = 1);
+
+private:
+    std::vector<SpawnPoint> spawn_points_;
+};
+
+// TODO
 // - spawn+despawn logic
 // - config
+// - random destination
 
 }  // namespace ts
